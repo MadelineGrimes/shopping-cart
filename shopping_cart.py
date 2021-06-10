c15ad64c27b258945b74618cffcfe3260ee0d443
Madeline Grimes <mkg420@stern.nyu.edu>
2021-06-10 00:17:04 -0400

    User Input

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,45 +1,119 @@
 import os
-import dotenv
+from dotenv import load_dotenv
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
-products = [
-    {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
-    {"id":2, "name": "All-Seasons Salt", "department": "pantry", "aisle": "spices seasonings", "price": 4.99},
-    {"id":3, "name": "Robust Golden Unsweetened Oolong Tea", "department": "beverages", "aisle": "tea", "price": 2.49},
-    {"id":4, "name": "Smart Ones Classic Favorites Mini Rigatoni With Vodka Cream Sauce", "department": "frozen", "aisle": "frozen meals", "price": 6.99},
-    {"id":5, "name": "Green Chile Anytime Sauce", "department": "pantry", "aisle": "marinades meat preparation", "price": 7.99},
-    {"id":6, "name": "Dry Nose Oil", "department": "personal care", "aisle": "cold flu allergy", "price": 21.99},
-    {"id":7, "name": "Pure Coconut Water With Orange", "department": "beverages", "aisle": "juice nectars", "price": 3.50},
-    {"id":8, "name": "Cut Russet Potatoes Steam N' Mash", "department": "frozen", "aisle": "frozen produce", "price": 4.25},
-    {"id":9, "name": "Light Strawberry Blueberry Yogurt", "department": "dairy eggs", "aisle": "yogurt", "price": 6.50},
-    {"id":10, "name": "Sparkling Orange Juice & Prickly Pear Beverage", "department": "beverages", "aisle": "water seltzer sparkling water", "price": 2.99},
-    {"id":11, "name": "Peach Mango Juice", "department": "beverages", "aisle": "refrigerated", "price": 1.99},
-    {"id":12, "name": "Chocolate Fudge Layer Cake", "department": "frozen", "aisle": "frozen dessert", "price": 18.50},
-    {"id":13, "name": "Saline Nasal Mist", "department": "personal care", "aisle": "cold flu allergy", "price": 16.00},
-    {"id":14, "name": "Fresh Scent Dishwasher Cleaner", "department": "household", "aisle": "dish detergents", "price": 4.99},
-    {"id":15, "name": "Overnight Diapers Size 6", "department": "babies", "aisle": "diapers wipes", "price": 25.50},
-    {"id":16, "name": "Mint Chocolate Flavored Syrup", "department": "snacks", "aisle": "ice cream toppings", "price": 4.50},
-    {"id":17, "name": "Rendered Duck Fat", "department": "meat seafood", "aisle": "poultry counter", "price": 9.99},
-    {"id":18, "name": "Pizza for One Suprema Frozen Pizza", "department": "frozen", "aisle": "frozen pizza", "price": 12.50},
-    {"id":19, "name": "Gluten Free Quinoa Three Cheese & Mushroom Blend", "department": "dry goods pasta", "aisle": "grains rice dried goods", "price": 3.99},
-    {"id":20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink", "department": "beverages", "aisle": "juice nectars", "price": 4.25}
-] # based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
+load_dotenv
 
-def to_usd(my_price):
-    """
-    Converts a numeric value to usd-formatted string, for printing and display purposes.
+#Beginning of Shopping Cart User Input
 
-    Param: my_price (int or float) like 4000.444444
-
-    Example: to_usd(4000.444444)
-
-    Returns: $4,000.44
-    """
-    return f"${my_price:,.2f}" #> $12,000.71
+#Store the results of the input process in the selected ID variable
+selected_id = input("Please enter a product ID: ") #Product ID is a string
+print(selected_id)
+print(type(selected_id))
 
 
-# TODO: write some Python code here to produce the desired output
+
+#Accept and print individual user input values
+#Use a while loop
+i = 1
+while i < 898248001107:
+    print(i)
+    if i == DONE:
+        break
+    i += 1
 
 
-print(os.getenv("TAX_RATE=0.0875"))
+#Use if/else statement with a break keyword once DONE is entered
 
-print(products)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Google API Authorization 
+DOCUMENT_ID = os.getenv("GOOGLE_SHEET_ID", default="OOPS")
+SHEET_NAME = os.getenv("SHEET_NAME", default="Products-2021")
+
+CREDENTIALS_FILEPATH = os.path.join(os.path.dirname(__file__), "shopping-cart", "google-credentials.json")
+
+AUTH_SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets", #> Allows read/write access to the user's sheets and their properties.
+    "https://www.googleapis.com/auth/drive.file" #> Per-file access to files created or opened by the app.
+]
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILEPATH, AUTH_SCOPE)
+print("CREDS:", type(credentials)) #> <class 'oauth2client.service_account.ServiceAccountCredentials'>
+
+client = gspread.authorize(credentials)
+print("CLIENT:", type(client))
+
+#Read sheet values
+
+print("-----------------")
+print("READING DOCUMENT...")
+
+doc = client.open_by_key(DOCUMENT_ID)
+print("DOC:", type(doc), doc.title) #> <class 'gspread.models.Spreadsheet'>
+
+sheet = doc.worksheet(SHEET_NAME)
+print("SHEET:", type(sheet), sheet.title)#> <class 'gspread.models.Worksheet'>
+
+rows = sheet.get_all_records()
+print("ROWS:", type(rows)) #> <class 'list'>
+
+for row in rows:
+    print(row)
+
+print("-----------------")
+print("NEW ROW...")
+
+auto_incremented_id = len(rows) + 898248001108
+new_row = {
+    "id": auto_incremented_id,
+    "name": f"Product {auto_incremented_id} (created from my python app)",
+    "department": "snacks",
+    "price": 4.99,
+    "availability_date": "2021-02-17"
+}
+print(new_row)
+
+print("-----------------")
+print("WRITING VALUES TO DOCUMENT...")
+new_values = list(new_row.values())
+next_row_number = len(rows) + 2
+response = sheet.insert_row(new_values, next_row_number)
+print("RESPONSE:", type(response))
+print(response)
+
