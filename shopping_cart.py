82533a6d0bb8ef8cc3d5a81a22db335458dc0db0
Madeline Grimes <mkg420@stern.nyu.edu>
2021-06-14 20:54:54 -0400

    Lookup Product w/User Input

--- shopping_cart.py
+++ shopping_cart.py
@@ -2,80 +2,17 @@ import os
 from dotenv import load_dotenv
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-
 load_dotenv
-
-#Beginning of Shopping Cart User Input
-
-#Store the results of the input process in the selected ID variable
-selected_id = input("Please enter a product ID: ") #Product ID is a string
-print(selected_id)
-print(type(selected_id))
-
-
-
-#Accept and print individual user input values
-#Use a while loop
-i = 1
-while i < 898248001107:
-    print(i)
-    if i == DONE:
-        break
-    i += 1
-
-
-#Use if/else statement with a break keyword once DONE is entered
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 #Google API Authorization 
-DOCUMENT_ID = os.getenv("GOOGLE_SHEET_ID", default="OOPS")
-SHEET_NAME = os.getenv("SHEET_NAME", default="Products-2021")
-
-CREDENTIALS_FILEPATH = os.path.join(os.path.dirname(__file__), "shopping-cart", "google-credentials.json")
-
+DOCUMENT_ID = "1ItN7Cc2Yn4K90cMIsxi2P045Gzw0y2JHB_EkV4mXXpI"
+SHEET_NAME = "Shopping Cart Project - Datastore (PUBLIC)"
+CREDENTIALS_FILEPATH = '/Users/madelinegrimes/Documents/GitHub/shopping-cart/google-credentials.json'
 AUTH_SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets", #> Allows read/write access to the user's sheets and their properties.
     "https://www.googleapis.com/auth/drive.file" #> Per-file access to files created or opened by the app.
 ]
-
 credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILEPATH, AUTH_SCOPE)
 print("CREDS:", type(credentials)) #> <class 'oauth2client.service_account.ServiceAccountCredentials'>
-
 client = gspread.authorize(credentials)
 print("CLIENT:", type(client))
 
@@ -116,4 +53,31 @@ next_row_number = len(rows) + 2
 response = sheet.insert_row(new_values, next_row_number)
 print("RESPONSE:", type(response))
 print(response)
+
+
+#Beginning of Shopping Cart User Input
+
+#Store the results of the input process in the selected ID variable
+selected_id = input("Please enter a product ID: ") #Product ID is a string
+#print(selected_id)
+#print(type(selected_id))
+#Look up the corresponding product from the list using list comprehension - filter items to a subset that match a certain condition
+matching_products = [p for p in SHEET_NAME if str(p["id"]) == str(selected_id)]
+matching_product = matching_products[0]
+print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"])
+#Remember to convert numbers to strings what concatinating them 
+
+
+#Accept and print individual user input values
+#Use a while loop
+i = 1
+while i < 898248001107:
+    print(i)
+    if i == DONE:
+        break
+    i += 1
+
+
+#Use if/else statement with a break keyword once DONE is entered
+
 
